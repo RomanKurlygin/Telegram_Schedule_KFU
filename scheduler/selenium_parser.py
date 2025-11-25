07702a088fe80ede153fbd3fbe5d824f00dbb971
RomanKurlygin <ksu23182002@gmail.com>
2025-11-25 21:43:48 +0300

    add imports

---
+++ scheduler/selenium_parser.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from config import CHROMEDRIVER_PATH
+
