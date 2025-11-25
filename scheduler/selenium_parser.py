0888260c20de1ae0b29892202a6a1fd60c047858
RomanKurlygin <ksu23182002@gmail.com>
2025-11-25 21:51:47 +0300

    add

--- scheduler/selenium_parser.py
+++ scheduler/selenium_parser.py
@@ -13,3 +13,13 @@ def get_schedule_kfu():
     options.add_argument('--disable-gpu')
     service = Service(CHROMEDRIVER_PATH)
     driver = webdriver.Chrome(service=service, options=options)
+
+    try:
+        url = "https://kpfu.ru/studentu/ucheba/raspisanie"
+        driver.get(url)
+
+        input_field = driver.find_element(By.CSS_SELECTOR, "input[type=text]")
+        input_field.clear()
+        input_field.send_keys(group_number)
+
+        search = driver.find_element(By.CSS_SELECTOR, "input[type=text]")
