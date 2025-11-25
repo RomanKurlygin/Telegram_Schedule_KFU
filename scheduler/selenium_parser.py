7764c969c4f66053d661097bde53754d664065f5
RomanKurlygin <ksu23182002@gmail.com>
2025-11-25 21:45:20 +0300

    add get schedule

--- scheduler/selenium_parser.py
+++ scheduler/selenium_parser.py
@@ -4,3 +4,12 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from config import CHROMEDRIVER_PATH
 
+def get_schedule_kfu():
+    """
+    Заходит на сайт КФУ, выбирает группу и получает расписание.
+    """
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    service = Service(CHROMEDRIVER_PATH)
+    driver = webdriver.Chrome(service=service, options=options)
