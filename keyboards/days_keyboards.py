2d0916ca0d345e508bb01150f938b7ef356adbe4
RomanKurlygin <ksu23182002@gmail.com>
2025-11-30 19:23:47 +0300

    add all week in keyboard

--- keyboards/days_keyboards.py
+++ keyboards/days_keyboards.py
@@ -3,5 +3,5 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def days_keyboard(group):
     kb = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text=d, callback_data=f"day_{group}_{d}") for d in ["Пн","Вт","Ср","Чт","Пт","Сб"]],
-
+        [InlineKeyboardButton(text="📅 Вся неделя", callback_data=f"week_{group}")],
     ])
