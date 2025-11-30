3e0fc871f6b2d9bf82191a86e5a9145f89709188
RomanKurlygin <ksu23182002@gmail.com>
2025-11-30 19:45:52 +0300

    bug fix

--- storage/save_load.py
+++ storage/save_load.py
@@ -11,19 +11,21 @@ def save_schedule(group, data):
     with open(SAVE_FILE, 'w', encoding="utf-8") as f:
         json.dump(file_data, f, ensure_ascii=False, indent=4)
 
-    def load_saved_schedule(group):
+def load_saved_schedule(group):
         if not os.path.exists(SAVE_FILE):
             return None
         with open(SAVE_FILE, 'r', encoding="utf-8") as f:
             data = json.load(f)
         return data.get(group)
 
-    def load_auto_users():
+def load_auto_users():
         if not os.path.exists(AUTO_FILE):
             return []
         with open(AUTO_FILE, 'r', encoding="utf-8") as f:
             return json.load(f)
 
-    def save_auto_users(users):
+def save_auto_users(users):
         with open(AUTO_FILE, 'w', encoding="utf-8") as f:
             json.dump(users, f, indent=4)
+
+
