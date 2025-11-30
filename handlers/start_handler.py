01c20e73d98698023e831f2c5e151560e7e42aa0
RomanKurlygin <ksu23182002@gmail.com>
2025-11-30 19:46:24 +0300

    add

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,8 +1,14 @@
 from aiogram import Router, types
 from aiogram.filters import Command
+from storage.save_load import load_saved_schedule
 
 router = Router()
 
 @router.message(Command("start"))
 async def start_cmd(msg: types.Message):
     await msg.answer("Введите номер группы (например 09-515):")
+
+@router.message()
+async def get_group(msg: types.Message):
+    group = msg.text.strip()
+    saved = load_saved_schedule(group)
