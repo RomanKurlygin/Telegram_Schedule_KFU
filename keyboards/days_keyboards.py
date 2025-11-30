ee5d5c89f685a91742c403c9afc07cd5e632ae07
RomanKurlygin <ksu23182002@gmail.com>
2025-11-30 19:24:23 +0300

    add auto and stop auto week in keyboard

--- keyboards/days_keyboards.py
+++ keyboards/days_keyboards.py
@@ -4,4 +4,8 @@ def days_keyboard(group):
     kb = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text=d, callback_data=f"day_{group}_{d}") for d in ["Пн","Вт","Ср","Чт","Пт","Сб"]],
         [InlineKeyboardButton(text="📅 Вся неделя", callback_data=f"week_{group}")],
+        [InlineKeyboardButton(text="⏰ Автоотправка", callback_data=f"auto_{group}"),
+         InlineKeyboardButton(text="⛔ Стоп авто", callback_data=f"stopauto_{group}")],
+
+
     ])
