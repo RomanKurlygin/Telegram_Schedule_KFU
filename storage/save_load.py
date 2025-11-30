1cc0ac2ffa2e039599e722d5db7461b83025b9c2
RomanKurlygin <ksu23182002@gmail.com>
2025-11-30 19:41:00 +0300

    add save

--- storage/save_load.py
+++ storage/save_load.py
@@ -1,2 +1,13 @@
 import json, os
 from config import SAVE_FILE, AUTO_FILE
+
+def save_schedule(group, data):
+    if os.path.exists(SAVE_FILE):
+        with open(SAVE_FILE, 'r', encoding="utf-8") as f:
+            file_data = json.load(f)
+    else:
+        file_data = {}
+    file_data[group] = data
+    with open(SAVE_FILE, 'w', encoding="utf-8") as f:
+        json.dump(file_data, f, ensure_ascii=False, indent=4)
+
